7e03f1a12c8040042c6373cc3761d7c98f2d04af
Jordan Hendricks <jordan.p.hendricks@gmail.com>
2016-01-30 13:16:28 -0500

    Added student stencil code for Chord.

---
+++ chord/chord/logging.go
@@ -0,0 +1,27 @@
+package chord
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var Debug *log.Logger
+var Out *log.Logger
+var Error *log.Logger
+
+// Initialize the loggers
+func init() {
+	Debug = log.New(ioutil.Discard, "", log.Ltime|log.Lshortfile)
+	Out = log.New(os.Stdout, "", log.Ltime|log.Lshortfile)
+	Error = log.New(os.Stdout, "ERROR: ", log.Ltime|log.Lshortfile)
+}
+
+// Turn debug on or off
+func SetDebug(enabled bool) {
+	if enabled {
+		Debug = log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	} else {
+		Debug = log.New(ioutil.Discard, "", log.Ldate|log.Ltime|log.Lshortfile)
+	}
+}
